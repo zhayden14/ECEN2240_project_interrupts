903e434df2277dfeddcd18bd45602b043a7fb8dd
zhayden14 <44064251+zhayden14@users.noreply.github.com>
2018-10-16 22:05:04 -0600

    Capture Inputs configured (untested)

--- TI_RSLK_controller_interrupts/main.c
+++ TI_RSLK_controller_interrupts/main.c
@@ -63,7 +63,7 @@ void main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
 
-       //configure Timer A 0
+       //configure Timer A0 (modify!)
        TIMER_A0->CCTL[0] = 0x0080;    // CCI0 toggle
        TIMER_A0->CCR[0] =  1000;    // Period or about 10 kHz
        TIMER_A0->EX0 =     0x0005;    // Divide by 3
@@ -73,23 +73,38 @@ void main(void)
        TIMER_A0->CCR[2] =  500;
        TIMER_A0->CTL =     0x0250;    // up mode, divide by 1
 
+       //configure Timer A1
+       TIMER_A1->CCTL[0] = 0x0080;        //compare mode
+       TIMER_A1->CCR[0] = 0xFFFF;    //maximum count range
+       TIMER_A1->CCTL[1] = 0x8900;//capture mode (falling edge), CCI1A, SCS=1,
+       TIMER_A1->CCTL[2] = 0x8900;//outmode: out bit, no interrupts
+       TIMER_A1->CCTL[3] = 0x8900;
+       TIMER_A1->CCTL[4] = 0x8900;
+       TIMER_A1->CTL = 0x0210;//SMCLK/1(3MHz), up mode, no interrupts
+
+       //configure Timer A2
+       TIMER_A2->CCTL[0] = 0x0080;
+       TIMER_A2->CCR[0] = 0xFFFF;
+       TIMER_A2->CCTL[1] = 0x8900;//capture mode (falling edge), CCI1A, SCS=1,
+       TIMER_A2->CCTL[2] = 0x8900;//outmode: out bit, no interrupts
+       TIMER_A2->CTL = 0x0210;
+
+       //configure Timer A3
+       TIMER_A3->CCTL[0] = 0x0080;    // CCI0 toggle
+       TIMER_A3->CCR[0] =  1000;    // Period of about 100 Hz
+       TIMER_A3->EX0 =     0x0002;    // Divide by 3
+       TIMER_A3->CCTL[1] = 0x0040;    // CCR1 toggle/reset
+       TIMER_A3->CCR[1] =  20;         //after 200 counts, GPIO->input
+       //TIMER_A3->CCTL[2] = 0x0040;
+       //TIMER_A3->CCR[2] =  500;
+       TIMER_A3->CTL =     0x0210;    // up mode, divide by 1
+
        //configure GPIO pins
        P2DIR = 0x3F;    //first 6 pins in port are outputs
        P2SEL0 = 0x30;   //pins 4 and 5 are PWM from TA0
        P2SEL1 &= ~0x3F;
        P2DS   = 0x3F;
        P2OUT &= 0x0F;
-
-       //configure Timer A3
-       TIMER_A3->CCTL[0] = 0x0080;    // CCI0 toggle
-       TIMER_A3->CCR[0] =  10000;    // Period or about 100 Hz
-       TIMER_A3->EX0 =     0x0002;    // Divide by 3
-       TIMER_A3->CCTL[1] = 0x0040;    // CCR1 toggle/reset
-       //TIMER_A3->CCTL[2] = 0x0040;
-       TIMER_A3->CCR[1] =  200;         //after 200 counts, GPIO->input
-       //TIMER_A3->CCR[2] =  500;
-       TIMER_A3->CTL =     0x0210;    // up mode, divide by 1
-
 
        //infinite loop
        while(1){}
